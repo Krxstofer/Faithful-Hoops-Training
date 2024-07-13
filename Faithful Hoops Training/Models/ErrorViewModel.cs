287f497be15ac9140c85e1c06205adfc9e1ad5ae
Kristofer Norman <kjn2418@email.vccs.edu>
2024-07-13 13:44:24 -0400

    Add project files.

---
+++ Faithful Hoops Training/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Faithful_Hoops_Training.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
