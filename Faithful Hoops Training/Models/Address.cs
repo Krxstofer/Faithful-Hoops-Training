783bc2ef3b959add6939f9e273c732425b9c8540
Kristofer Norman <kjn2418@email.vccs.edu>
2024-07-16 23:25:38 -0400

    fixing some small syntactical errors.

--- Faithful Hoops Training/Models/Address.cs
+++ Faithful Hoops Training/Models/Address.cs
@@ -6,8 +6,8 @@ namespace FaithfulHoopsTraining.Models
     {
         [Key]
         public int Id { get; set; }
-        public int String { get; set; }
-        public int City { get; set; }
-        public int State { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
     }
 }
