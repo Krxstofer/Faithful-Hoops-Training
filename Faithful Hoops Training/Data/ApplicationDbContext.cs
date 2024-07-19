a6c4237c604008821eb607e7a5d03cb8b4f0fb8c
Kristofer Norman <kjn2418@email.vccs.edu>
2024-07-18 23:59:43 -0400

    Creating some database context members

--- Faithful Hoops Training/Data/ApplicationDbContext.cs
+++ Faithful Hoops Training/Data/ApplicationDbContext.cs
@@ -1,7 +1,17 @@
-﻿namespace FaithfulHoopsTraining.Data
-{
-    public class ApplicationDbContext
-    {
+﻿using FaithfulHoopsTraining.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace FaithfulHoopsTraining.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<HoopSession> HoopSessions { get; set; }
+        public DbSet<TrainSession> TrainSessions { get; set; }
+        public DbSet<Address> Addresses { get; set; }
     }
 }
