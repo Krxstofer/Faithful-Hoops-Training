13cdc68643604252d6788f1608d1541bb942c222
Kristofer Norman <kjn2418@email.vccs.edu>
2024-07-22 19:09:13 -0400

    Adding Controllers.

---
+++ Faithful Hoops Training/Controllers/HoopSessionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FaithfulHoopsTraining.Controllers
+{
+    public class HoopSessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Faithful Hoops Training/Controllers/TrainSessionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FaithfulHoopsTraining.Controllers
+{
+    public class TrainSessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
