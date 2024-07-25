f21f9f0ca8398f11f2803454c04d372024ed7979
Kristofer Norman <kjn2418@email.vccs.edu>
2024-07-25 01:41:30 -0400

    Implementing Data to my View

--- Faithful Hoops Training/Controllers/HoopSessionController.cs
+++ Faithful Hoops Training/Controllers/HoopSessionController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FaithfulHoopsTraining.Data;
+using FaithfulHoopsTraining.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FaithfulHoopsTraining.Controllers
 {
     public class HoopSessionController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HoopSessionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<HoopSession> hoopSessions = _context.HoopSessions.ToList();
+            return View(hoopSessions);
         }
     }
 }
