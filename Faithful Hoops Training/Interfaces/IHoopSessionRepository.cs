4273001c436db2ac7e5d4747383e8efb8c00231e
Kristofer Norman <kjn2418@email.vccs.edu>
2024-08-11 23:09:00 -0400

    Creating Interfaces.

---
+++ Faithful Hoops Training/Interfaces/IHoopSessionRepository.cs
@@ -0,0 +1,15 @@
+﻿using FaithfulHoopsTraining.Models;
+
+namespace FaithfulHoopsTraining.Interfaces
+{
+    public interface IHoopSessionRepository
+    {
+        Task<IEnumerable<HoopSession>> GetAll();
+        Task<HoopSession> GetByIdAsync(int id);
+        Task<IEnumerable<HoopSession>> GetHoopSessionsByCity(string city);
+        bool Add(HoopSession hoopSession);
+        bool Update(HoopSession hoopSession);
+        bool Delete(HoopSession hoopSession);
+        bool Save();
+    }
+}
---
+++ Faithful Hoops Training/Interfaces/ITrainSessionRepository.cs
@@ -0,0 +1,15 @@
+﻿using FaithfulHoopsTraining.Models;
+
+namespace FaithfulHoopsTraining.Interfaces
+{
+    public interface ITrainSessionRepository
+    {
+        Task<IEnumerable<TrainSession>> GetAll();
+        Task<TrainSession> GetByIdAsync(int id);
+        Task<IEnumerable<TrainSession>> GetTrainSessionsByCity(string city);
+        bool Add(TrainSession trainSession);
+        bool Update(TrainSession trainSession);
+        bool Delete(TrainSession trainSession);
+        bool Save();
+    }
+}
