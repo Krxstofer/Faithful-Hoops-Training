2d62e1b084d1d8f4eb7a6be3dd387a65b4a59347
Kristofer Norman <kjn2418@email.vccs.edu>
2024-08-12 21:43:44 -0400

    ADDIBNG REPOSITORIES

---
+++ Faithful Hoops Training/Repository/HoopSessionRepository.cs
@@ -0,0 +1,50 @@
+﻿using FaithfulHoopsTraining.Data;
+using FaithfulHoopsTraining.Interfaces;
+using FaithfulHoopsTraining.Models;
+
+namespace FaithfulHoopsTraining.Repository
+{
+    public class HoopSessionRepository : IHoopSessionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HoopSessionRepository(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        public bool Add(HoopSession hoopSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(HoopSession hoopSession)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<HoopSession>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HoopSession> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<HoopSession>> GetHoopSessionsByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(HoopSession hoopSession)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
